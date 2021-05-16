002757917e53ffa664dd360a1c3492beb1014ac5
siamahajani9 <83707461+siamahajani9@users.noreply.github.com>
2021-05-16 17:52:00 +0530

    It declares the Array of a String and Integer.

---
+++ Declaration_of_Arrays.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {  
+Scanner sun = new Scanner (System.in);
+String[] arr = {"Sia is good","Dodo is Sia's dog","Sia has a nice family"};
+int[] oarr = {99,89,79,69};
+System.out.println(arr [0] + "," +  arr [1] + "," + arr [2] );
+System.out.println(oarr [0] + "," + oarr [1] + "," + oarr [2] + "," + oarr [3]);
+  }
+}
